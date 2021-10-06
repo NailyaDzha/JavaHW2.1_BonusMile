929786858a3d2ca9a293ff81b29c0c119eb4b949
Nailya <botan4091@gmail.com>
2021-10-06 20:50:30 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketprice = 25000;
+        int bonusmileprice = 10;
+        int totalbonus = ticketprice / bonusmileprice;
+        System.out.println(totalbonus);
+    }
+}
